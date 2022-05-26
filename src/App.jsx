ec998a92ae53d1130a037beee16e93fa2c89bea2
Chase Riggs <criggstx@gmail.com>
2022-05-26 11:23:03 -0500

    first rough draft

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import DesktopMenu from './desktopMenu.jsx';
 import Services from './services.jsx';
 import About from './about.jsx';
+import Contact from './contact.jsx';
 import './App.css'
 
 function App() {
@@ -22,35 +23,7 @@ function App() {
       <DesktopMenu/>
       <Services/>
       <About/>
-        <p>
-          <a
-            className="App-link"
-            href="https://www.instagram.com/archuletavirtual/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Follow me on Instagram
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://www.facebook.com/ArchuletaVirtual/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Follow me on Facebook
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://calendly.com/archuletavirtual/30min?fbclid=IwAR1tCgfdbn0oIRfHbRdFz_8_SL2tnfEnqtFPiKwy97bfHV68J874EP9KCjk&month=2022-05"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Schedule a free consultation
-          </a>
-        </p>
-
+      <Contact/>
     </div>
   )
   }
---
+++ src/contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function contact () {
+
+  return (
+    <div className='contact-container'>
+      <div className='contact-title'>
+        CONTACT
+      </div>
+      <div className='contact-blurb'>
+        We want to know what makes you tick.
+      </div>
+      <div className='facebook'>
+        <a href='https://www.facebook.com/ArchuletaVirtual/' target="_blank">
+          <img className='contact-image' src="https://i.imgur.com/4RRUVuq.jpg" alt="follow us on facebook" />
+          </a>
+      </div>
+      <div className='instagram'>
+      <a href='https://www.instagram.com/archuletavirtual/' target="_blank">
+        <img className='contact-image' src="https://i.imgur.com/3k5wbqB.jpg" alt="follow us on instagram" />
+        </a>
+      </div>
+      <div className='calendar'>
+        <a href='https://calendly.com/archuletavirtual/30min?fbclid=IwAR1tCgfdbn0oIRfHbRdFz_8_SL2tnfEnqtFPiKwy97bfHV68J874EP9KCjk&month=2022-05' target="_blank">
+        <img className='contact-image' src="https://i.imgur.com/es5VFfs.jpg" alt="schedule a consultation" />
+        </a>
+      </div>
+    </div>
+  )
+}
